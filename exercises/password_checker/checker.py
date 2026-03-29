820e9fe4891e17ae817fa6f857dcceee53662048
jacobjacobjacobjacobjacob <jacob.schjetne@gmail.com>
2026-03-29 23:44:21 +0200

    comment

--- exercises/password_checker/checker.py
+++ exercises/password_checker/checker.py
@@ -1,5 +1,7 @@
 
 """
 Simple password checker that evaluates password strength based on common passwords, length, special characters etc. ≈
-# Password wordlist sourced from:
-# https://github.com/danielmiessler/SecLists/blob/master/Passwords/Common-Credentials/10k-most-common.txt
+
+Password wordlist sourced from:
+https://github.com/danielmiessler/SecLists/blob/master/Passwords/Common-Credentials/10k-most-common.txt
+"""
